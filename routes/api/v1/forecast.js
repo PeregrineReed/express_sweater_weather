683c1e1321824772cbce36b723016c041f0c0006
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-01 13:49:45 -0600

    Updated forecast endpoint to return the weather

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,39 +1,35 @@
 var express = require('express');
-// var fetch = require('node-fetch');
-// var City = require('../../../models').City;
+var fetch = require('node-fetch');
 var pry = require('pryjs');
 
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-// router.get('/', async function(req, res, next) {
-//   const geocode = async () => {
-//     try {
-//       const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=AIzaSyDZXAjAoJTlZwO24HRUR1HNy-GkmdEx9fQ`);
-//       const data = await response.json();
-//       return data;
-//     } catch (error) {
-//       return error;
-//     }
-//   };
-//   results = await geocode();
-//   coordinates = results.results[0].geometry.location;
+router.get('/', async function(req, res, next) {
+  const geocode = async () => {
+    try {
+      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=AIzaSyDZXAjAoJTlZwO24HRUR1HNy-GkmdEx9fQ`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error;
+    }
+  };
+  var location = await geocode();
+  var coordinates = location.results[0].geometry.location;
 
-  eval(pry.it);
-  // City.findOne({
-  //   where: {
-  //     name: req.query.location
-  //   }
-  // })
-  // .then(user => {
-  //   res.setHeader("Content-Type", "application/json");
-  //   res.status(201).send(JSON.stringify({data: 'forecast'}))
-  // })
-  // .catch(error => {
-  //   res.setHeader("Content-Type", "application/json");
-  //   res.status(401).send({ error });
-  // });
-  res.send('forecast')
+  const forecast = async () => {
+    try {
+      const response = await fetch(`https://api.darksky.net/forecast/821bdc0c5e50220cb45d84d29cbddbbb/${coordinates.lat},${coordinates.lng}`);
+      const data = await response.json()
+      return data;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  var weather = await forecast();
+  res.setHeader("Content-Type", "application/json");
+  res.status(201).send(JSON.stringify(weather))
 });
 
 module.exports = router;
