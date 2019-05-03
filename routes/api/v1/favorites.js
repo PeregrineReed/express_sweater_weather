1176756b61bc42a7b9d1556777e6d1c2ef2eb0ee
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-03 07:56:00 -0600

    Added favoritesRouter

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -6,7 +6,31 @@ var User = require('../../../models').User;
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
+  User.findOne({
+    where: {
+      apiKey: req.body.apiKey
+    }
+  }).then(user => {
+    user.cities 
+  }).then(results => {
+    res.setHeader("Content-Type", "application/json")
+    res.status(200).send(JSON.stringify({message: `${req.body.location} has been added to your favorites`}))
+  }).catch(error => {
+    res.status(401).send({ error })
+  })
+});
 
+router.get('/', function(req, res, next) {
+  User.findOne({
+    where: {
+      apiKey: req.body.apiKey
+    }
+  }).then(user => {
+    res.setHeader("Content-Type", "application/json")
+    res.status(200).send(user.cities)
+  }).catch(error => {
+    res.status(401).send({ error })
+  })
 });
 
 module.exports = router;
