6281084a65378dccc1b823f6faf349725170193f
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-04 16:08:04 -0600

    Updated post and get for favorites

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -6,20 +6,58 @@ var City = require('../../../models').City;
 
 var router = express.Router();
 
-router.post('/', function(req, res, next) {
-  User.findOne({
+router.post('/', async function(req, res, next) {
+  var user = await User.findOne({
     where: {
       apiKey: req.body.apiKey
     }
-  }).then(user => {
-    // user.cities
+  });
 
-  }).then(results => {
-    res.setHeader("Content-Type", "application/json")
-    res.status(200).send(JSON.stringify({message: `${req.body.location} has been added to your favorites`}))
-  }).catch(error => {
-    res.status(401).send({ error })
+  var location = await geocode();
+  const geocode = async () => {
+    try {
+      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=${process.env.GEOCODE_KEY}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  var city = await City.findOrCreate({
+    where: {
+      name: location.results[0].address_components[0].long_name,
+      state: location.results[0].address_components[2].short_name,
+      country: location.results[0].address_components[3].long_name
+    },
+    defaults: {
+      name: location.results[0].address_components[0].long_name,
+      state: location.results[0].address_components[2].short_name,
+      country: location.results[0].address_components[3].long_name,
+      lat: location.results[0].geometry.location.lat,
+      long: location.results[0].geometry.location.lng
+    },
+  });
+
+  var favorite = await UserCity.findOrCreate({
+    where: {
+      UserId: user.id,
+      CityId: city.id
+    },
+    defaults: {
+      UserId: user.id,
+      CityId: city.id
+    }
   })
+
+  res.setHeader("Content-Type", "application/json");
+  if (user && user.apiKey === req.body.apiKey) {
+    res.status(201).send(JSON.stringify({
+      message: `${city.name}, ${city.state}, ${city.country} has been added to your favorites.`
+    }));
+  } else {
+    res.status(401).send(JSON.stringify({error: 'Something went wrong'}))
+  }
 });
 
 router.get('/', function(req, res, next) {
